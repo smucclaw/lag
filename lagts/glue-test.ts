31ff3508b50bd617af21ea84862ef7c149d7e718
Inari Listenmaa <inari.listenmaa@gmail.com>
2024-06-12 09:53:56 +0200

    WIP: trying to glue together previous components (#1)
    * Added my own tests that so far just work with constant strings
    
    * copied over the interns' code from long time ago to test
    
    * trying out tojson.interpret but doesn't seem that helpful
    
    * use actual bools, not strings "true" and "false"
    
    * evaluate Drinks() expression first to give value to "drinks"
    
    * cleanup and comments

---
+++ lagts/glue-test.ts
@@ -0,0 +1,62 @@
+
+import * as tsm from '../../usecases/sect10-typescript/src/mathlang';
+import { setup, exprReduce, myshow, Qualifies, Drinks, extendRecord } from './generated';
+
+const mengs_output = { "index":0
+, "message":
+    { "role":"assistant"
+    , "content":null
+    , "tool_calls":
+        [
+            { "id":"call_SSrGnyCScXJLqOMP5dgHAL8F"
+            , "type":"function"
+            , "function":
+                { "name":"compute_qualifies"
+                , "arguments": {
+                    "walks": "true",
+                    "eats": "false",
+                    "drinks": "unknown",
+                    "beverage type": "non-alcoholic",
+                    "in whole": "true"
+                    }
+                }
+            }
+        ]
+    }
+, "logprobs":null
+, "finish_reason":"tool_calls"
+}
+
+function get_values(input: any) {
+    return input["message"]["tool_calls"][0]["function"]["arguments"];
+}
+
+
+// This is copied and simplified from usecases/sect10-typescript/crunch.ts
+// Maybe there's a better place for it???
+function go(user_input:any) {
+    setup(user_input); // modifies global variable tsm.symTab
+
+    // evaluating Drinks gives us new values in a dictionary
+    const drinks = Drinks()
+    const asDict = exprReduce(drinks)
+
+    // we extend the symtab (of user inputs) with the newly evaluated Drinks
+    extendRecord(tsm.symTab, {...asDict})
+
+    // with the value of "drinks" coming from Drinks(), we can now evaluate Qualifies
+    const expr = Qualifies()
+
+
+    // const exprval = asDict.testPoint ? asDict.testPoint : expr.val
+    console.log("The whole expression evaluates to:", expr.val, "\n\n")
+
+    myshow(expr)
+
+    console.log("** reduced expr as dict")
+    console.log (`#+BEGIN_SRC js`)
+    console.log(JSON.stringify(exprReduce(expr), null, 2))
+    console.log (`#+END_SRC`)
+}
+
+go(get_values(mengs_output))
