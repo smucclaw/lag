f971c248b7bbe0b1f93039a3338967e8c32098bc
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2024-06-14 15:54:10 +0800

    Add google TypeScript style

--- lagts/src/generated.ts
+++ lagts/src/generated.ts
@@ -1,82 +1,89 @@
 import * as tsm from 'sect10-typescript/src/mathlang';
-export { exprReduce, asDot } from 'sect10-typescript/src/mathlang';
+export {exprReduce, asDot} from 'sect10-typescript/src/mathlang';
 
 // sets up a global variable tsm.symTab. Also copied from Meng's usecases code
-export function setup (symtab : any) {
-    const transformedJson = transformJson(symtab);
-    return tsm.initSymTab(transformedJson)
-  }
+export function setup(symtab: any) {
+  const transformedJson = transformJson(symtab);
+  return tsm.initSymTab(transformedJson);
+}
 
 // This is a temporary hack, just to make the Must Sing 5 example work.
 // TODO: once we have a robust pipeline from
 // sreadsheet -> schema -> LLM outputs values for user input using that same schema
 // then we don't need to massage the schema because it will already have the same field names
-function transformJson(input: { [key: string]: any }): { [key: string]: any } {
-    const result: { [key: string]: any } = {};
+function transformJson(input: {[key: string]: any}): {[key: string]: any} {
+  const result: {[key: string]: any} = {};
 
-    for (const key in input) {
-        if (input.hasOwnProperty(key)) {
-            const value = input[key];
+  for (const key in input) {
+    if (input.hasOwnProperty(key)) {
+      const value = input[key];
 
-            // Check if the value is true, false or unknown
-            if (String(value) === "true" || String(value) === "false") {
-                result[key] = (value === "true") ;
-            } else if (value === "unknown") {
-                result[key] = undefined ;
-            } else {
-                // If value is none of these, transform it to a key with value "true"
-                result[value] = true;
-            }
-        }
+      // Check if the value is true, false or unknown
+      if (String(value) === 'true' || String(value) === 'false') {
+        result[key] = value === 'true';
+      } else if (value === 'unknown') {
+        result[key] = undefined;
+      } else {
+        // If value is none of these, transform it to a key with value "true"
+        result[value] = true;
+      }
     }
-    return result;
+  }
+  return result;
 }
 
 // copied from Meng's old code in usecases and shortened
-export function myshow(expr: tsm.Expr<any>) : tsm.Expr<any> {
-    console.log("** " + expr.val)
-    tsm.explTrace(expr, 3)
+export function myshow(expr: tsm.Expr<any>): tsm.Expr<any> {
+  console.log('** ' + expr.val);
+  tsm.explTrace(expr, 3);
 
-    console.log("** JSON of symTab")
-    console.log("#+NAME symtab")
-    console.log("#+BEGIN_SRC json")
-    console.log(JSON.stringify(tsm.symTab,null,2))
-    console.log("#+END_SRC")
-    return expr
-  }
+  console.log('** JSON of symTab');
+  console.log('#+NAME symtab');
+  console.log('#+BEGIN_SRC json');
+  console.log(JSON.stringify(tsm.symTab, null, 2));
+  console.log('#+END_SRC');
+  return expr;
+}
 
 // used to extend the symtab after evaluating different expressions
-export function extendRecord(existing: Record<string, any>, updates: Record<string, any>): Record<string, any> {
-    for (const key in updates) {
-        if (updates.hasOwnProperty(key)) {
-            if (existing[key] === undefined) {
-                existing[key] = updates[key];
-            }
-        }
+export function extendRecord(
+  existing: Record<string, any>,
+  updates: Record<string, any>
+): Record<string, any> {
+  for (const key in updates) {
+    if (updates.hasOwnProperty(key)) {
+      if (existing[key] === undefined) {
+        existing[key] = updates[key];
+      }
     }
-    return existing;
   }
+  return existing;
+}
 
 // TODO: these should be added here by transpiling the spreadsheet
 // this thing is actually generated
-export const Qualifies = () => {return new tsm.BoolFold ( "Qualifies"
-                 , tsm.BoolFoldOp.All
-                 , [ new tsm.GetVar ("walks")
-                     , new tsm.BoolFold ( "any/all"
-                                        , tsm.BoolFoldOp.All
-                                        , [ new tsm.BoolFold ( "any/all"
-                                                             , tsm.BoolFoldOp.Any
-                                                             , [ new tsm.GetVar ("drinks")
-                                                                 , new tsm.GetVar ("eats") ] ) ] ) ] )}
-export const Drinks = () => {return new tsm.BoolFold ( "drinks"
-                 , tsm.BoolFoldOp.All
-                 , [ new tsm.BoolFold ( "any/all"
-                                      , tsm.BoolFoldOp.Any
-                                      , [ new tsm.GetVar ("alcoholic")
-                                          , new tsm.GetVar ("non-alcoholic") ] )
-                     , new tsm.BoolFold ( "any/all"
-                                        , tsm.BoolFoldOp.All
-                                        , [ new tsm.BoolFold ( "any/all"
-                                                             , tsm.BoolFoldOp.Any
-                                                             , [ new tsm.GetVar ("in part")
-                                                                 , new tsm.GetVar ("in whole") ] ) ] ) ] )}
+export const Qualifies = () => {
+  return new tsm.BoolFold('Qualifies', tsm.BoolFoldOp.All, [
+    new tsm.GetVar('walks'),
+    new tsm.BoolFold('any/all', tsm.BoolFoldOp.All, [
+      new tsm.BoolFold('any/all', tsm.BoolFoldOp.Any, [
+        new tsm.GetVar('drinks'),
+        new tsm.GetVar('eats'),
+      ]),
+    ]),
+  ]);
+};
+export const Drinks = () => {
+  return new tsm.BoolFold('drinks', tsm.BoolFoldOp.All, [
+    new tsm.BoolFold('any/all', tsm.BoolFoldOp.Any, [
+      new tsm.GetVar('alcoholic'),
+      new tsm.GetVar('non-alcoholic'),
+    ]),
+    new tsm.BoolFold('any/all', tsm.BoolFoldOp.All, [
+      new tsm.BoolFold('any/all', tsm.BoolFoldOp.Any, [
+        new tsm.GetVar('in part'),
+        new tsm.GetVar('in whole'),
+      ]),
+    ]),
+  ]);
+};
--- lagts/src/openai-test.ts
+++ lagts/src/openai-test.ts
@@ -1,40 +1,46 @@
-import OpenAI from "openai";
+import OpenAI from 'openai';
 
 const openai = new OpenAI();
 
 const chatTool: OpenAI.ChatCompletionTool = {
-  type: "function",
+  type: 'function',
   function: {
-    "name": "compute_qualifies",
-    "description": "Determines if a person qualifies for the purposes of the rule. The input object describes the person's properties in the primary parameters: walks, eats, drinks. Secondary parameters can be given which are sufficient to determine some of the primary parameters. A person drinks whether or not they consume an alcoholic or a non-alcoholic beverage, in part or in whole; those specific details don't really matter. The output of the function can be either a request for required information; a restatement of the user input requesting confirmation prior to function calling; or a Boolean answer with optional explanation summary.",
-    "parameters": {
-      "type": "object",
-      "properties": {
-        "walks": {
-          "type": "string", "enum": ["true", "false", "unknown"],
-          "description": "Did the person walk?",
+    name: 'compute_qualifies',
+    description:
+      "Determines if a person qualifies for the purposes of the rule. The input object describes the person's properties in the primary parameters: walks, eats, drinks. Secondary parameters can be given which are sufficient to determine some of the primary parameters. A person drinks whether or not they consume an alcoholic or a non-alcoholic beverage, in part or in whole; those specific details don't really matter. The output of the function can be either a request for required information; a restatement of the user input requesting confirmation prior to function calling; or a Boolean answer with optional explanation summary.",
+    parameters: {
+      type: 'object',
+      properties: {
+        walks: {
+          type: 'string',
+          enum: ['true', 'false', 'unknown'],
+          description: 'Did the person walk?',
         },
-        "eats": {
-          "type": "string", "enum": ["true", "false", "unknown"],
-          "description": "Did the person eat?",
+        eats: {
+          type: 'string',
+          enum: ['true', 'false', 'unknown'],
+          description: 'Did the person eat?',
         },
-        "drinks": {
-          "type": "string", "enum": ["true", "false", "unknown"],
-          "description": "Did the person drink?",
+        drinks: {
+          type: 'string',
+          enum: ['true', 'false', 'unknown'],
+          description: 'Did the person drink?',
         },
-        "beverage type": {
-          "type": "string", "enum": ["alcoholic", "non-alcoholic", "unknown"],
-          "description": "Did the person drink an alcoholic beverage?",
+        'beverage type': {
+          type: 'string',
+          enum: ['alcoholic', 'non-alcoholic', 'unknown'],
+          description: 'Did the person drink an alcoholic beverage?',
         },
-        "in whole": {
-          "type": "string", "enum": ["true", "false", "unknown"],
-          "description": "Did the person drink all of the beverage?",
+        'in whole': {
+          type: 'string',
+          enum: ['true', 'false', 'unknown'],
+          description: 'Did the person drink all of the beverage?',
         },
       },
-      "required": ["walks"],
-    }
-  }
-}
+      required: ['walks'],
+    },
+  },
+};
 
 // EVERY Person
 //  WHO  walks
@@ -42,12 +48,15 @@ const chatTool: OpenAI.ChatCompletionTool = {
 //    OR drinks
 //  MUST sing
 
-
 async function main() {
   const completion = await openai.chat.completions.create({
     messages: [
-      { role: "system", content: "You are a helpful assistant. Some queries will require calling an external function. Importantly, don't make assumptions about what values to plug into functions! Ask for clarification if a user request does not give sufficient data. After the facts have been collected, do not attempt to reason through the logic; instead, always call the function to evaluate the logic and obtain an answer. When delivering the final answer to the user, give a detailed explanation." },
-      { role: "user", content: "Hi! Help me determine if a person qualifies." },
+      {
+        role: 'system',
+        content:
+          "You are a helpful assistant. Some queries will require calling an external function. Importantly, don't make assumptions about what values to plug into functions! Ask for clarification if a user request does not give sufficient data. After the facts have been collected, do not attempt to reason through the logic; instead, always call the function to evaluate the logic and obtain an answer. When delivering the final answer to the user, give a detailed explanation.",
+      },
+      {role: 'user', content: 'Hi! Help me determine if a person qualifies.'},
 
       //    "tool_calls": [
       //      {
@@ -59,16 +68,32 @@ async function main() {
       //        }
       //      }
 
-      { role: "function", name: "compute_qualifies", content: "require (walks AND (eats OR drinks))" },
+      {
+        role: 'function',
+        name: 'compute_qualifies',
+        content: 'require (walks AND (eats OR drinks))',
+      },
 
-      { role: "assistant", content: "I need some additional information to determine if the person qualifies. Could you please provide the following details:\n\n1. Did the person walk? (true/false/unknown)\n2. Did the person eat? (true/false/unknown)\n3. Did the person drink? (true/false/unknown)" },
+      {
+        role: 'assistant',
+        content:
+          'I need some additional information to determine if the person qualifies. Could you please provide the following details:\n\n1. Did the person walk? (true/false/unknown)\n2. Did the person eat? (true/false/unknown)\n3. Did the person drink? (true/false/unknown)',
+      },
 
-      { role: "user", content: "I know they did not walk." },
+      {role: 'user', content: 'I know they did not walk.'},
 
-      { role: "assistant", content: "Thank you for that information. \n\nCan you confirm if the person ate? (true/false/unknown)" },
+      {
+        role: 'assistant',
+        content:
+          'Thank you for that information. \n\nCan you confirm if the person ate? (true/false/unknown)',
+      },
       // wrong! should be asking if the person ate or drank.
 
-      { role: "user", content: "I don't know if they ate. I know they drank a beverage but I don't know if it was alcoholic or not." },
+      {
+        role: 'user',
+        content:
+          "I don't know if they ate. I know they drank a beverage but I don't know if it was alcoholic or not.",
+      },
 
       // erroneously? assumes that drinks is unknown
       // "tool_calls": [
@@ -82,20 +107,25 @@ async function main() {
       //  }
       // ]
 
-      // { role: "function", 
+      // { role: "function",
       // 	name: "compute_qualifies",
       // 	arguments: "{\"walks\":\"false\",\"drinks\":\"true\",\"beverage type\":\"unknown\"}"
       // },
 
-      { role: "function", name: "compute_qualifies", content: "{ \"restatement\": {\"walks\":\"false\",\"drinks\":\"true\",\"eats\":\"false\"} }" },
-
       {
-        role: "assistant",
-        content: "Based on the information you've provided:\n- The person did not walk.\n- It's unknown if the person ate.\n- The person drank a beverage, but it is uncertain whether it was alcoholic or not.\n\nLet's confirm with the following statement before I proceed with determining if the person qualifies: \nThe person did not walk, they drank a beverage, and we don't know if they ate. Is this correct?"
+        role: 'function',
+        name: 'compute_qualifies',
+        content:
+          '{ "restatement": {"walks":"false","drinks":"true","eats":"false"} }',
       },
 
-      { role: "user", content: "Yes, those facts are correct." },
+      {
+        role: 'assistant',
+        content:
+          "Based on the information you've provided:\n- The person did not walk.\n- It's unknown if the person ate.\n- The person drank a beverage, but it is uncertain whether it was alcoholic or not.\n\nLet's confirm with the following statement before I proceed with determining if the person qualifies: \nThe person did not walk, they drank a beverage, and we don't know if they ate. Is this correct?",
+      },
 
+      {role: 'user', content: 'Yes, those facts are correct.'},
 
       //   "tool_calls": [
       //    {
@@ -111,15 +141,19 @@ async function main() {
       // the rule engine runs logic in a defensible, auditable, transparent way to return the top-level answer.
       // the LLM then generates an explanation guided by the logic engine.
 
-      { role: "function", name: "compute_qualifies", content: "{ \"answer\": \"false\", \"explanation\": { \"necessary(walks)\" } }" },
+      {
+        role: 'function',
+        name: 'compute_qualifies',
+        content: '{ "answer": "false", "explanation": { "necessary(walks)" } }',
+      },
 
       {
-        role: "assistant",
-        content: "Based on the information provided, the person does not qualify because they did not walk. \n\nThe rule requires the person to have walked and then to have either eaten or drunk something. Since the person did not walk, they do not meet the necessary conditions."
-      }
-
+        role: 'assistant',
+        content:
+          'Based on the information provided, the person does not qualify because they did not walk. \n\nThe rule requires the person to have walked and then to have either eaten or drunk something. Since the person did not walk, they do not meet the necessary conditions.',
+      },
     ],
-    model: "gpt-4o",
+    model: 'gpt-4o',
     tools: [chatTool],
   });
 
